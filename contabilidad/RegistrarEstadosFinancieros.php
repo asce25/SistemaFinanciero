f5cf9ffe8ca83a9585b4f40c90426648a0dcfb7a
asce25 <asce25@gmail.com>
2014-12-03 13:13:22 -0600

    Version 1

---
+++ contabilidad/RegistrarEstadosFinancieros.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+  <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+        <link href="../css_net/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="../css_net/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <!-- Ionicons -->
+        <link href="../css_net/ionicons.min.css" rel="stylesheet" type="text/css" />
+        <!-- Theme style -->
+        <link href="../css/AdminLTE.css" rel="stylesheet" type="text/css" />
+</head>
+
+ <body class="skin-blue">
+ <div class="col-md-4" style="width:650px;">
+     <div class="box box-solid">
+         <div class="box-header">
+         <h3 class="box-title">Agregar a Catálogo</h3>
+         </div>
+         <div class="box-body">
+         <table width="600" border="0" cellpadding="10px">
+  <tr>
+    <td><div class="input-group">
+         <label>Nombre de la Cuenta</label>
+         <input type="text" class="form-control" placeholder="Ingreso nombre de la cuenta">
+         </div></td>
+    <td width="200" valign="bottom">
+    <label>Saldo</label>
+    <div class="input-group">
+    
+                                        <span class="input-group-addon">$</span>
+                                        <input type="text" class="form-control">
+                                        <span class="input-group-addon">.00</span>
+                                    </div></td>
+  </tr>
+</table>
+
+         
+       </div>
+     </div>
+ </div>
+</body>
+</html>
